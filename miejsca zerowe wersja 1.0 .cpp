45b08f166f0302feafe1dd228d115cd356c03594
Patryk <patryk.banaczyk@gmail.com>
2021-10-26 15:30:42 +0200

    wersja czwarta

--- miejsca zerowe wersja 1.0 .cpp
+++ miejsca zerowe wersja 1.0 .cpp
@@ -2,12 +2,32 @@
 
 using namespace std;
 
-void miejsca_zerowe(int a, b)
+void miejsca_zerowe(float a, float b)
 
 {
-	int wartosc;
+	float wartosc;
+	
+	cout<<"Twoja funkcja ma wartosc"<<a<<"x+a ("<<b<<") = 0"<<endl;
+	if(a==0 && b!=0)
+	{
+		cout<<"Nie mozna utworzyc funkcji liniowej"<<endl;
+	}
+	
+	if(a==0 && b==0)
+	{
+		cout<<"Funkcja ma nieskonczenie wiele miejsc zerowych"<<endl;
+		
+		else
+		{
+			wartosc = -b/a;
+			cout<<"Miejsce zerowe funkcji to: "<<wartosc<<endl;
+		}
+	}
+	
 	cout<<"Podaj liczbe a: "<<endl;
 	cin>>a;
 	cout<<"Podaj liczbe b: "<<endl;
 	cin>>b;
+	
+	
 }
