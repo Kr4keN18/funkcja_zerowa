2aefc3f40b6dc2371f9372729d1805f334f9e360
Patryk <patryk.banaczyk@gmail.com>
2021-10-26 15:13:18 +0200

    wersja trzecia

--- miejsca zerowe wersja 1.0 .cpp
+++ miejsca zerowe wersja 1.0 .cpp
@@ -1,1 +1,13 @@
-Ala ma kota
+#include <iostream>
+
+using namespace std;
+
+void miejsca_zerowe(int a, b)
+
+{
+	int wartosc;
+	cout<<"Podaj liczbe a: "<<endl;
+	cin>>a;
+	cout<<"Podaj liczbe b: "<<endl;
+	cin>>b;
+}
